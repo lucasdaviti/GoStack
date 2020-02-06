8dc480af353e3afd0c4ae8db574e98b5cffbc9e6
Lucas Davi <lucasdavisan@gmail.com>
2020-02-06 15:34:52 -0300

    Learning about query params & route params.

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+// Query params => ?teste=1
+// Route params => /users/1
+// Request body => { "name": "Diego", "email": "rocketseat@suporte.com.br" }
+
+server.get('/users/:id', (req, res) => {
+  // const nome = req.query.nome;
+  const { id } = req.params;
+
+  return res.json({ message: `Buscando o usuário, ${id}!` });
 });
 
 server.listen(3000);
