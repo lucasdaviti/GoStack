55910f1188650ac93d53cab15b70e399442142fd
Lucas Davi <lucasdavisan@gmail.com>
2020-02-06 18:39:23 -0300

    Creating updating route.

--- challenge01/index.js
+++ challenge01/index.js
@@ -24,4 +24,15 @@ server.get('/projects', (req, res) => {
   return res.json(projectList);
 });
 
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const searchedProject = projectList.find(p => p.id == id);
+
+  searchedProject.title = title;
+
+  return res.json(searchedProject);
+});
+
 server.listen(3000);
