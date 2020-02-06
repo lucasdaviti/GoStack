685c5aec5a2f112da0f4725dcc291bf973d8db87
Lucas Davi <lucasdavisan@gmail.com>
2020-02-06 19:06:40 -0300

    Creating excluding route.

--- challenge01/index.js
+++ challenge01/index.js
@@ -35,4 +35,14 @@ server.put('/projects/:id', (req, res) => {
   return res.json(searchedProject);
 });
 
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projectList.findIndex(p => p.id == id);
+
+  projectList.splice(projectIndex, 1);
+
+  return res.status(200).json({ message: 'Project deleted succeed!' });
+});
+
 server.listen(3000);
